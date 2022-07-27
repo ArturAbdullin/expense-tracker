6a99d58109f66ea7d2761433524a26db479e0c7b
Artur Abdullin <artur.abdullin@gmail.com>
2022-07-27 17:23:02 +0300

    feat(ExpenseItem): present a date in month-year-day format

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -8,9 +8,23 @@ interface ExpenseItemProps extends React.HTMLAttributes<JSX.Element> {
 }
 
 function ExpenseItem(props: ExpenseItemProps) {
+  const month: string =
+    props.date?.toLocaleString("en-US", {
+      month: "long",
+    }) || "";
+  const year: string = props.date?.getFullYear().toString() || "";
+  const day: string =
+    props.date?.toLocaleString("en-US", {
+      day: "2-digit",
+    }) || "";
+
   return (
     <div className="expense-item">
-      <div>{props.date?.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
